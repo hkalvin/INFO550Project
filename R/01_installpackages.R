6452ea6c97c1ce4165a809867e580d41a014f595
Hannah Kalvin <hannah.kalvin@emory.edu>
2020-10-21 18:47:20 -0500

    fix mirror issue in package installation

--- R/01_installpackages.R
+++ R/01_installpackages.R
@@ -5,7 +5,7 @@ installed_pkgs <- row.names(installed.packages())
 pkgs <- c("readr", "ggplot2","tidyverse","magrittr","kableExtra")
 for(p in pkgs){
   if(!(p %in% installed_pkgs)){
-    install.packages(p)
+    install.packages(p, , repos="http://cran.us.r-project.org")
   }
 }
 
